e2ebd199c07e8b68ad37d8838fff1eada8173a12
RauanKani <148891795+RauanKani@users.noreply.github.com>
2023-10-24 23:48:06 +0600

    Add files via upload

---
+++ task 2 K.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <cmath>
+
+int main() {
+    int row_kon, col_kon, row_figura, col_figura;
+    std::cin >> row_kon >> col_kon >> row_figura >> col_figura;
+
+    int row_diff = abs(row_kon - row_figura);
+    int col_diff = abs(col_kon - col_figura);
+
+    if ((row_diff == 1 && col_diff == 2) || (row_diff == 2 && col_diff == 1)) {
+        std::cout << "YES" << std::endl;
+    }
+    else {
+        std::cout << "NO" << std::endl;
+    }
+
+    return 0;
+}
---
+++ task 2 R.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+
+int main() {
+    int k, m, n;
+    std::cin >> k >> m >> n;
+
+    int time = ((n * 2 * m) + k - 1) / k;
+    std::cout << time << std::endl;
+
+    return 0;
+}
