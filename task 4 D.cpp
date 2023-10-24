aed9f356581bca4331a0a6cd8751287dd169d871
RauanKani <148891795+RauanKani@users.noreply.github.com>
2023-10-24 23:55:01 +0600

    task 4

---
+++ task 4 D.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+int main() {
+    int N;
+    std::cin >> N;
+
+    int prev, current;
+    int count = 0;
+
+    std::cin >> prev; 
+
+    for (int i = 1; i < N; ++i) { 
+        std::cin >> current;
+        if (current > prev) {
+            count++;
+        }
+        prev = current;
+    }
+
+    std::cout << count << std::endl;
+
+    return 0;
+}
