b207597d16c4438d7fd9233cfd1405cd6e0ffe52
RauanKani <148891795+RauanKani@users.noreply.github.com>
2023-10-24 23:53:24 +0600

    Add files via upload

---
+++ task 1 D.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    int N, K;
+    cin >> N >> K;
+    int remaining_apples = K % N;
+    cout << remaining_apples << endl;
+
+    return 0;
+}
